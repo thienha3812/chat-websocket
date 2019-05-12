09cd954975672ce17526fc376add3f3803eac5a1
Macintosh HD <thienhamaimai1@gmail.com>
2019-05-13 04:46:14 +0700

    123

--- server.js
+++ server.js
@@ -74,7 +74,6 @@ wss.on('connection', function connection(ws) {
                         json: true
                     }, function (err, response) { // Response decode
                         if (err) console.log(err);
-                        console.log(response)
                         if (response.statusCode === 200) {
                             if (socketIdMap.has(ws.uuid)) {
                                 ws.CONNECTING = true
